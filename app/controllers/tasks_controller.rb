d394d115e371a5a48775d0d8dea8c46bf540f4d6
Nick Exposito <nexpos@gmail.com>
2017-12-18 14:10:25 -0700

    first commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,53 @@
+class TasksController < ApplicationController
+  before_action :all_tasks, only: [:index, :create, :update, :destroy]
+  before_action :set_task, only: [:edit, :update, :destroy]
+
+  # GET /tasks
+  # GET /tasks.json
+
+  # GET /tasks/1
+  # GET /tasks/1.json
+
+  # GET /tasks/new
+  def new
+    @task = Task.new
+  end
+
+  # GET /tasks/1/edit
+
+  # POST /tasks
+  # POST /tasks.json
+  def create
+    @task = Task.create(task_params)
+  end
+
+  def update
+    @task.update(task_params)
+  end
+
+  def destroy
+    @task.destroy
+  end
+
+  # PATCH/PUT /tasks/1
+  # PATCH/PUT /tasks/1.json
+
+
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
+
+  private
+
+    def all_tasks
+      @tasks = Task.order(deadline: :asc)
+    end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def task_params
+      params.require(:task).permit(:description, :deadline)
+    end
+end
